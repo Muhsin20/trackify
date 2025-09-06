99510799aa4d38a47075b4a20705702a0d48d79a
Muhsin <madan6@uic.edu>
2025-09-06 16:29:44 -0500

    Upgraded Package Versions

--- jest.config.ts
+++ jest.config.ts
@@ -1,12 +1,23 @@
-export default {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest",
-  },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
+import nextJest from 'next/jest';
+import type { Config } from 'jest';
+
+const createJestConfig = nextJest({ dir: './' });
+
+const config: Config = {
+  testEnvironment: 'jest-environment-jsdom',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  clearMocks: true,            // optional, but helpful
+  restoreMocks: true, 
   moduleNameMapper: {
-    "^@/(.*)$": "<rootDir>/src/$1",
+    // Handle module aliases if you use them, e.g. "@/components/*"
+    '^@/(.*)$': '<rootDir>/$1',
+    // Mock CSS modules / files
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    // Mock static file imports (images, svgs, etc.)
+    '\\.(gif|ttf|eot|svg|png|jpe?g|webp)$': '<rootDir>/test/__mocks__/fileMock.js',
   },
-  setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+  // If some ESM deps need transforming, add them here (example):
+  // transformIgnorePatterns: ['/node_modules/(?!some-esm-package)'],
 };
+
+export default createJestConfig(config);
